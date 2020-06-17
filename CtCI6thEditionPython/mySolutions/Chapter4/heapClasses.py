8eb8fc72be90eac7c30a2f6e1ddc25a58fc41b26
Nathan <nathanf49@gmail.com>
2020-06-17 16:26:30 -0400

    Before Vacation

--- CtCI6thEditionPython/mySolutions/Chapter4/heapClasses.py
+++ CtCI6thEditionPython/mySolutions/Chapter4/heapClasses.py
@@ -7,6 +7,28 @@ class minHeap:
                 root = binaryNode(root)
             else:
                 raise TypeError("Root must be a numerical value")
+        else: # make sure root contains a number
+            if type(root.value) is not int and type(root.value) is not float:
+                raise TypeError("Root node must contain a number")
         self.root = root
+
+    def addNode(self, newNode):
+        if type(newNode) is not binaryNode: # if input is not a binary node, attempt to cast it if value is a number
+            if type(newNode) is int or type(newNode) is float:
+                newNode = binaryNode(newNode)
+            else:
+                TypeError("The new node must be a binary node containing a number")
+        else: # if new Node is a binary Node, make sure it contains a number
+            if type(newNode.value) is not int and type(newNode.value) is not float:
+                raise TypeError("New node must contain a number")
+
+
+    def removeMin(self):
+
+    def findOpen(self): #finds the first node with a position to allow a child
+        current = self.root
+        while len(current.children) == 2: # while current level of graph is full, move to next level
+            current = current.children[1]
+
 
 x= minHeap(5)
